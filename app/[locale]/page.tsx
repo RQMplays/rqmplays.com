53ec16eaa2ea900f45d8bbfd059fefc7debb7d46
Benoit 'RQM-' Laurent <blaurent@makemusic.com>
2024-05-26 16:19:07 +0200

    set images as background with parallax animation

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,9 @@
 import styles from './page.module.css'
+import Image from 'next/image'
+import ux1 from '@/public/ux1.jpg'
+import espace from '@/public/espace2.jpg'
+import spectacle from '@/public/spectacle2.jpg'
+import drums from '@/public/drums.jpg'
 import { useTranslations } from 'next-intl'
 
 export default function Home() {
@@ -11,29 +16,67 @@ export default function Home() {
         <div className={styles.name}>Benoit</div>, mais tu me retrouvera sur la
         toile sous le pseudonyme de <div className={styles.nickname}>RQM.</div>
       </div>
-      <div className={styles.paragraphs}>
+      <div className={styles.container}>
+        <Image className={styles.image} src={ux1} alt="ux" fill />
+        <div className={styles.paragraph}>
+          Je suis un développeur web front-end avec une grande sensibilité pour
+          l'<div className={styles.red}>UX</div> et le{' '}
+          <div className={styles.yellow}>design</div> en général.
+        </div>
+      </div>
+      <div className={styles.container}>
+        <Image className={styles.image} src={espace} alt="espace" fill />
+        <div className={styles.paragraph}>
+          Issu d'un parcours attypique, j'ai tout de suite été tres attiré par
+          le design et particulièrement le design d'
+          <div className={styles.red}>espace</div>.
+        </div>
+      </div>
+      <div className={styles.container}>
+        <Image className={styles.image} src={spectacle} alt="spectacle" fill />
+        <div className={styles.paragraph}>
+          Ni trouvant pas assez de fantaisie j'ai commencé à étudier les arts du{' '}
+          <div className={styles.yellow}>spectacle</div>, théatre et cinéma.
+        </div>
+      </div>
+      <div className={styles.container}>
+        <Image className={styles.image} src={drums} alt="drums" fill />
+        <div className={styles.paragraph}>
+          C'est la que la musique est entré dans ma vie, j'ai alors étudié la
+          sonorisation de spectacles vivants, et notamment de{' '}
+          <div className={styles.red}>concerts</div>.
+        </div>
+      </div>
+      {/* <div className={styles.paragraphs}>
         <div className={styles.one}>
           Je suis un développeur web front-end avec une grande sensibilité pour
-          l'UX et le design en général.
+          l'<div className={styles.red}>UX</div> et le{' '}
+          <div className={styles.yellow}>design</div> en général.
+        </div>
+        <div className={styles.uxImageContainer}>
+          <Image className={styles.uxImage} src={ux1} alt="ux" fill />
         </div>
         <div className={styles.two}>
           Issu d'un parcours attypique, j'ai tout de suite été tres attiré par
-          le design et particulièrement le design d'espace.
+          le design et particulièrement le design d'
+          <div className={styles.red}>espace</div>.
         </div>
         <div className={styles.three}>
-          Ni trouvant pas assez de fantaisie j'ai commencé à étudier les arts du
-          spectacles, théatres et cinéma.
+          Ni trouvant pas assez de fantaisie j'ai commencé à étudier les arts du{' '}
+          <div className={styles.yellow}>spectacle</div>, théatre et cinéma.
         </div>
         <div className={styles.four}>
           C'est la que la musique est entré dans ma vie, j'ai alors étudié la
-          sonorisation de spectacles vivants, et notamment de concerts.
+          sonorisation de spectacles vivants, et notamment de{' '}
+          <div className={styles.red}>concerts</div>.
         </div>
         <div className={styles.five}>
           Prenant gout au défi technique, je me suis découvert une passion pour
-          le développement web, et spécialement le front-end pour ce savant
-          mélange entre techniques et artistiques.
+          le développement web, et spécialement le{' '}
+          <div className={styles.yellow}>front-end</div> pour ce savant mélange
+          entre techniques et artistiques.
         </div>
-      </div>
+      </div> */}
     </main>
   )
 }
